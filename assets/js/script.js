2f2af2339822806d22e6b9b9966954d09ebd52d1
gaillynnn <gaillynnneilson@gmail.com>
2024-04-28 18:01:54 -0500

    Added the month date and year

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,21 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    $("#todo-cards").append(`
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+   
+      <div class="card-body">
+      <h5 class="card-title">${dayjs(task.dueDate).format("MM/DD/YYYY")}</h5>
+        <h5 class="card-title">${task.title}</h5>
+        <p class="card-text"-> ${task.description} </p>
+      </div>
+    </div>
+  </div>
+</div>
 
+`)
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -18,20 +32,22 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+function handleAddTask(event) {
     event.preventDefault()
-   var task = {
-    title:$("#title").val(),
-    description:$("#description").val(),
-    dueDate:$("#duedate").val(), 
-   }
-   console.log(task,"TASK",taskList)
-   taskList.push(task);
-   localStorage.setItem("tasks", JSON.stringify(taskList));
+    var task = {
+        title: $("#title").val(),
+        description: $("#description").val(),
+        dueDate: $("#duedate").val(),
+        status:"todo"
+    }
+    console.log(task, "TASK", taskList)
+    taskList.push(task);
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    createTaskCard(task)
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
 
 }
 
@@ -42,5 +58,5 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-   $("#savetask").on("click", handleAddTask);
+    $("#savetask").on("click", handleAddTask);
 });
